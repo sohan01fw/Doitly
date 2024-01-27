42134a37aa1423a5875feac1e242575337428fde
sohancwact <sohancwactfw@gmail.com>
2024-01-27 20:03:21 +0545

    removing all prisma config

--- Backend/src/Controller/UserController.js
+++ Backend/src/Controller/UserController.js
@@ -1,28 +1,13 @@
-import { asyncHandler } from "../../utils/aysncHandler.js";
-import cookieToken from "../../utils/cookieToken.js";
-import { prisma } from "./../../prisma/index.js";
-
-export const createUserController = asyncHandler(async (req, res) => {
+export const createUserController = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log(req.body);
     if (!name || !email || !password) {
       res.status(404).json({ msg: "no email or password or name are found" });
     }
-
-    const User = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
-    cookieToken(User, res);
   } catch (error) {
-    console.log(error);
     throw new Error(error);
   }
-});
+};
 export const getUserController = async (req, res) => {
   res.send("hey this is users");
 };
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,19 +4,22 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { TodosRouter } from "./Routes/TodoRoutes.js";
 import { userRouter } from "./Routes/UserRoutes.js";
+
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//cookie middleware
-app.use(cookieParser());
+//Third-party middleware
 app.use(cors());
-app.use("/api/v1/todos", TodosRouter);
-app.use("/api/v1/user", userRouter);
+app.use(cookieParser());
+
 app.get("", (req, res) => {
   res.send("hello welcome to doitly");
 });
+app.use("/api/v1/todos", TodosRouter);
+app.use("/api/v1/user", userRouter);
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });

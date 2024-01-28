5b34ecb8b09c21137fd5204e98307a1156abc8b5
sohancwact <sohancwactfw@gmail.com>
2024-01-28 11:49:52 +0545

    successfully setuping prisma

--- Backend/src/Controller/UserController.js
+++ Backend/src/Controller/UserController.js
@@ -1,9 +1,19 @@
+import { prisma } from "../../prisma/index.js";
+
 export const createUserController = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
       res.status(404).json({ msg: "no email or password or name are found" });
     }
+    const userRes = await prisma.user.create({
+      data: {
+        name: "sohan",
+        email: "sohan@gmail.com",
+        password: "sohnq123",
+      },
+    });
+    console.log(userRes);
   } catch (error) {
     throw new Error(error);
   }

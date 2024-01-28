503a2e7390c9c98ad62f9bb445c143c325073cc6
sohancwact <sohancwactfw@gmail.com>
2024-01-28 17:55:54 +0545

    learning some advanced error handeling concept

---
+++ Backend/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse extends Response {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}

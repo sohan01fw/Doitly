93a21f46c78977536b30022b4bb0671c5ab0ee7d
sohancwact <sohancwactfw@gmail.com>
2024-02-06 21:38:20 +0545

    adding nextauth auth options

--- To-do-App/app/api/auth/[...nextauth]/route.ts
+++ To-do-App/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    // Google provider
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     // ...add more providers here
   ],
 };
